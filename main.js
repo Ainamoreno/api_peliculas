92bb056e5e58df58294ed8570d584e2d81d42c9f
Aina <ainamoreno@outlook.com>
2022-08-04 19:33:12 +0200

    Método search

--- main.js
+++ main.js
@@ -1,43 +1,53 @@
 const cargarPeliculasPopulares = async () => {
-    try {
-        const respuesta = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=b5138e06a3a9125b8c326498bbeae997&language=en-ES&page=1')
-    
-      const datos =  await respuesta.json();
-      let peliculas = '';
-       datos.results.forEach(pelicula => {
-        peliculas += `
-            <div class="pelicula">
-                <img class="poster" src='https://image.tmdb.org/t/p/w500/${pelicula.poster_path}'>
-                <h3 class="titulo" >${pelicula.title}</h3>
-            </div>
-            `;
-       });
-       document.getElementById('contenedor').innerHTML = peliculas
-
-    } catch (error) {
-        console.log(error);
-    }
-
+    cargarPeliculas('https://api.themoviedb.org/3/movie/popular?api_key=b5138e06a3a9125b8c326498bbeae997&language=es-ES&page=1')
 }
 const cargarPeliculasProximas = async () => {
+    cargarPeliculas('https://api.themoviedb.org/3/movie/upcoming?api_key=c4a520e4e71ab0a5e2f83a4b3c9aac47&language=es-ES&page=1')
+}
+
+const cargarPeliculas = async (url) => {
+
     try {
-        const respuesta = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=c4a520e4e71ab0a5e2f83a4b3c9aac47&language=en-ES&page=1')
-        
-      const datos =  await respuesta.json();
-      let peliculas = '';
-       datos.results.forEach(pelicula => {
-        peliculas += `
+        const respuesta = await fetch(url)
+
+        const datos = await respuesta.json();
+        let peliculas = '';
+        datos.results.forEach(pelicula => {
+            peliculas += `
             <div class="pelicula">
                 <img class="poster" src='https://image.tmdb.org/t/p/w500/${pelicula.poster_path}'>
                 <h3 class="titulo" >${pelicula.title}</h3>
-            </div>
-            `;
-       });
-       document.getElementById('contenedor').innerHTML = peliculas
+            </div>`;
+        });
+        document.getElementById('contenedor').innerHTML = peliculas;
 
     } catch (error) {
         console.log(error);
     }
 
+
 }
+const search = async (event) => {
+    let valor = event.target.value;
+    if (valor) {
+        try {
+            const busqueda = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=c4a520e4e71ab0a5e2f83a4b3c9aac47&language=es-ES&page=1&include_adult=false&query=${event.target.value}`)
+            const resultado = await busqueda.json();
+            let peliculas = '';
+            resultado.results.forEach(pelicula => {
+                peliculas += `
+            <div class="pelicula">
+                <img class="poster" src='https://image.tmdb.org/t/p/w500/${pelicula.poster_path}'>
+                <h3 class="titulo">${pelicula.title}</h3>
+            </div>`
+            });
+            document.getElementById('contenedor').innerHTML = peliculas;
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+
 
